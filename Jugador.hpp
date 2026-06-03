95502e137822ae6661632e14b49505b5f8625b62
armdo25 <villyarmando2410@gmail.com>
2026-06-02 21:37:47 -0600

    Add files via upload

---
+++ Jugador.hpp
@@ -0,0 +1,16 @@
+#ifndef JUGADOR_HPP
+#define JUGADOR_HPP
+
+#include "Usuario.hpp"
+#include "EventoDeportivo.hpp"
+#include "Apuesta.hpp"
+
+class Jugador : public Usuario {
+public:
+    Jugador(int idUsuario, string nombre, double saldo);
+
+    Apuesta realizarApuesta(int idApuesta, EventoDeportivo evento, double monto, string seleccion);
+    void verHistorial(vector<Apuesta>& apuestas) override;
+};
+
+#endif
